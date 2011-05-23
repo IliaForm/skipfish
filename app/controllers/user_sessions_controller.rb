f1356acc92fdd2980c01e81a08e75f2cb5db5158
Ilia Formalsky <i.formalsky@obersun.ru>
2011-05-23 15:19:24 +0400

    поправил logout

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -19,7 +19,7 @@
   def destroy
     current_user_session.destroy
     #flash[:notice] = "Logout successful!"
-    redirect_back_or_default new_user_session_url
+    redirect_to new_user_session_url
   end
 end
 
