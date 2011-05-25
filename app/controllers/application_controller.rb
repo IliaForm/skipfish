bfcb63f021c0fdc1afe42811445ef1388e3c8968
Ilia Formalsky <i.formalsky@obersun.ru>
2011-05-25 14:44:31 +0400

    e-pay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,7 @@ class ApplicationController < ActionController::Base
   helper :all
   helper_method :current_user_session, :current_user
   
-    def not_found
-    	render :template => '/shared/404'
-    end
-  
+
   private
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
@@ -47,6 +44,9 @@ class ApplicationController < ActionController::Base
       session[:return_to] = nil
     end
     
-
+    def not_found
+    	render :template => '/shared/404'
+    end
+  
 end
 
